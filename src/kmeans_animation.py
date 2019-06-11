bf128f467d2efc017ca28c27969cec3e853d09c1
nguyenngoclinhchi <nguyenngoclinhchi@u.nus.edu>
2019-06-11 10:11:24 +0800

    no message

--- src/kmeans_animation.py
+++ src/kmeans_animation.py
@@ -1,8 +1,9 @@
-import numpy as np 
+import numpy as np
 import matplotlib.pyplot as plt
 from scipy.spatial.distance import cdist
 import matplotlib.animation as animation
 import random
+
 np.random.seed(4)
 
 means = [[2, 2], [8, 3], [3, 6]]
@@ -11,8 +12,9 @@ X0 = np.random.multivariate_normal(means[0], cov, 500)
 X1 = np.random.multivariate_normal(means[1], cov, 500)
 X2 = np.random.multivariate_normal(means[2], cov, 500)
 
-X = np.concatenate((X0, X1, X2), axis = 0)
+X = np.concatenate((X0, X1, X2), axis=0)
 K = 3
+
 
 def kmeans_init_centers(X, k):
     # randomly pick k rows of X
@@ -22,26 +24,28 @@ def kmeans_init_centers(X, k):
 
 def kmeans_assign_labels(X, centers):
     # calculate pairwise distances btw data and centers
-    D = cdist(X, centers)
+    d = cdist(X, centers)
     # return index of the closest center
-    return np.argmin(D, axis = 1)
-    
+    return np.argmin(d, axis=1)
+
 
 def kmeans_update_centers(X, labels, K):
     centers = np.zeros((K, X.shape[1]))
     for k in range(K):
         Xk = X[labels == k, :]
-        centers[k,:] = np.mean(Xk, axis = 0)
+        centers[k, :] = np.mean(Xk, axis=0)
     return centers
+
 
 def has_converged(centers, new_centers):
     return (set([tuple(a) for a in centers]) == set([tuple(a) for a in new_centers]))
+
 
 def kmeans(X, K):
     centers = [kmeans_init_centers(X, K)]
     labels = []
     max_it = 100
-    it = 0 
+    it = 0
     while it < max_it:
         labels.append(kmeans_assign_labels(X, centers[-1]))
         new_centers = kmeans_update_centers(X, labels[-1], K)
@@ -51,15 +55,17 @@ def kmeans(X, K):
         it += 1
     return (centers, labels, it)
 
-(centers, labels, it) = kmeans(X, K)
-print centers[-1]
-print labels[-1].shape
 
+(centers, labels, it) = kmeans(X, K)
+print(centers[-1])
+print(labels[-1].shape)
 
 ###########
-from matplotlib.animation import FuncAnimation 
+from matplotlib.animation import FuncAnimation
 from scipy.spatial import Voronoi, voronoi_plot_2d
+
 vor = Voronoi(centers[-1])
+
 
 def voronoi_finite_polygons_2d(vor, radius=None):
     """
@@ -119,7 +125,7 @@ def voronoi_finite_polygons_2d(vor, radius=None):
 
             # Compute the missing endpoint of an infinite ridge
 
-            t = vor.points[p2] - vor.points[p1] # tangent
+            t = vor.points[p2] - vor.points[p1]  # tangent
             t /= np.linalg.norm(t)
             n = np.array([-t[1], t[0]])  # normal
 
@@ -133,7 +139,7 @@ def voronoi_finite_polygons_2d(vor, radius=None):
         # sort region counterclockwise
         vs = np.asarray([new_vertices[v] for v in new_region])
         c = vs.mean(axis=0)
-        angles = np.arctan2(vs[:,1] - c[1], vs[:,0] - c[0])
+        angles = np.arctan2(vs[:, 1] - c[1], vs[:, 0] - c[0])
         new_region = np.array(new_region)[np.argsort(angles)]
 
         # finish
@@ -141,19 +147,22 @@ def voronoi_finite_polygons_2d(vor, radius=None):
 
     return new_regions, np.asarray(new_vertices)
 
+
 fig, ax = plt.subplots()
 
+
 def update(ii):
-    label2 = 'iteration {0}: '.format(ii/2)
-    if ii%2:
+    label2 = 'iteration {0}: '.format(ii / 2)
+    if ii % 2:
         label2 += ' update centers'
     else:
         label2 += ' assign points to clusters'
 
-    i_c = (ii+1)/2 
-    i_p = ii/2
+    i_c = (ii + 1) / 2
+    print(ii/2)
+    i_p = ii / 2
 
-    label = labels[i_p]
+    label = labels[int(i_p)]
     X0 = X[label == 0, :]
     X1 = X[label == 1, :]
     X2 = X[label == 2, :]
@@ -166,28 +175,31 @@ def update(ii):
     animlist = plt.plot(X0[:, 0], X0[:, 1], 'b^', **kwargs)
     animlist = plt.plot(X1[:, 0], X1[:, 1], 'go', **kwargs)
     animlist = plt.plot(X2[:, 0], X2[:, 1], 'rs', **kwargs)
+    plt.show()
 
     # display centers and voronoi 
-    i = i_c 
+    i = int(i_c)
 
     kwargs = {"markersize": 15, "markeredgecolor": 'k'}
     animlist = plt.plot(centers[i][0, 0], centers[i][0, 1], 'y^', **kwargs)
     animlist = plt.plot(centers[i][1, 0], centers[i][1, 1], 'yo', **kwargs)
     animlist = plt.plot(centers[i][2, 0], centers[i][2, 1], 'ys', **kwargs)
+    plt.show()
 
     colors = ['b', 'g', 'r']
     ## vonoroi 
     points = centers[i]
     vor = Voronoi(points)
-    regions, vertices = voronoi_finite_polygons_2d(vor, radius = 1000)
-    for i,region in enumerate(regions):
+    regions, vertices = voronoi_finite_polygons_2d(vor, radius=1000)
+    for i, region in enumerate(regions):
         polygon = vertices[region]
-        animlist = plt.fill(*zip(*polygon), alpha=.2, color = colors[i])
-
+        animlist = plt.fill(*zip(*polygon), alpha=.2, color=colors[i])
 
     ax.set_xlabel(label2)
     return animlist, ax
 
-anim = FuncAnimation(fig, update, frames=np.arange(0, 2*it), interval=1000)
-# anim.save('kmeans.gif', dpi=200, writer='imagemagick')
+
+anim = FuncAnimation(fig, update, frames=np.arange(0, 2 * it), interval=1000)
+anim.save('kmeans.gif', dpi=200, writer='imagemagick')
 plt.show()
+
